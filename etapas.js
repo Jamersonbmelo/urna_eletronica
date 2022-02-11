7e3faed9962b2f878fa5d81ed4130b67900a16be
Jamerson <jamersonjosealvaro@gmail.com>
2022-02-11 15:51:33 -0300

    meu primeiro commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 45555,
+                nome: 'Jamerson Melo',
+                partido: 'PSDB',
+                fotos: [
+                    { url: 'jamerson.jpg', legenda: 'Vereador', }
+                ]
+            },
+            {
+                numero: 22123,
+                nome: 'Victor Hugo',
+                partido: 'PL',
+                fotos: [
+                    { url: 'victorhugo.jpg', legenda: 'Vereador', }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 45,
+                name: 'Silvio Mendes',
+                partido: 'PSDB',
+                vice: 'Iracema Portella',
+                fotos: [
+                    { url: 'silvio.jpg', legenda: 'Prefeito' },
+                    { url: 'iracema.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: 13,
+                name: 'Rafael Fonteles',
+                partido: 'PP',
+                vice: 'Júlio Cesar',
+                fotos: [
+                    { url: 'rafaelfonteles.jpg', legenda: 'Prefeito' },
+                    { url: 'juliocesar.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
+
