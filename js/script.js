fd72b993cec13ac0d7fefed915ce2627a2df07b5
unknown <jamersonjosealvaro@gmail.com>
2022-02-14 15:28:27 -0300

    script atualizado

--- js/script.js
+++ js/script.js
@@ -5,8 +5,27 @@ let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
+let etapaAtual = 0;
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++) {
+        numeroHtml += '<div class="numero"></div>';
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
 function clicou(n) {
-    alert("Clicou em "+n);
+    alert("Clicou em " + n);
 }
 function branco() {
     alert("Clicou em BRANCO!");
@@ -17,3 +36,4 @@ function corrige() {
 function confirma() {
     alert("Clicou em CONFIRMA!");
 }
+comecarEtapa();
