12d8072c29bf137d3163dd155d3312d913a6c738
unknown <jamersonjosealvaro@gmail.com>
2022-02-14 10:01:38 -0300

    Organizando Projeto

--- js/script.js
+++ js/script.js
@@ -4,3 +4,4 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+let 
